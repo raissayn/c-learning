fd18cbe905cfe8fc59566d11692463403cb73327
Raissa Nunes <raissan084@gmail.com>
2025-04-30 19:04:41 -0300

    revisando

---
+++ revisando.cpp
@@ -0,0 +1,25 @@
+// introdução / revisão
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    // declaração de variáveis 
+    int idade;
+    idade = 20;
+    cout << "Idade: " << idade << endl;
+    // operadores
+    int n1, n2, resultado;
+    n1 = 10;
+    n2 = 20;
+    resultado = n1 + n2;
+    cout << "Resultado: " << resultado << endl;
+    // resultado float, uma das variaveis tem que ser float
+    // resultado = (float)n1/n2 
+    int divResultado;
+    divResultado = n2 % n1; // % resto divisão
+    cout << "Resultado RESTO divisão: " << divResultado << endl;
+    // n++ = n+1
+    return 0;
+}
