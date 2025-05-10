dec94d88ee77af5228302f1b6173c7afbc322221
Raissa Nunes <raissan084@gmail.com>
2025-05-09 22:46:36 -0300

    att

--- att.cpp
+++ att.cpp
@@ -5,31 +5,44 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 // laços (for, while, do-while) - Implemente um contador crescente e um decrescente com loops
-// while= repete enquanto a condição for verdadeira, do-while= executa o bloco pelo menos uma vez, e só depois verifica a condição.
-cout << "Contador de 0 á 10 de forma crescente: "<< endl;
-for( int i = 0; i < 10; i++){
-    cout << i << endl;
-    i++;
-}
-cout << "Contador de 0 á 10 de forma decrescente : "<< endl;
-int j = 10;
-while ( j >= 1 ){
-    cout << j << endl;
-    j--;
-}
+// // while= repete enquanto a condição for verdadeira, do-while= executa o bloco pelo menos uma vez, e só depois verifica a condição.
+// cout << "Contador de 0 á 10 de forma crescente: "<< endl;
+// for( int i = 0; i < 10; i++){
+//     cout << i << endl;
+//     i++;
+// }
+// cout << "Contador de 0 á 10 de forma decrescente : "<< endl;
+// int j = 10;
+// while ( j >= 1 ){
+//     cout << j << endl;
+//     j--;
+// }
 
-// vetores - Crie um programa que leia e imprima 5 notas armazenadas em um vetor
-int vetor [5];
-for(int i = 0; i < 5; i++){
-    cout << "Digite o valor para armazenar no vetor, posição " <<  i << "º : " << endl;
-    cin >> vetor [i];
-}
-for(int i = 0; i < 5; i++){
-    cout << " Valor armazenado na posição " << i << "º : " << vetor[i]<< endl;
-}
+// // vetores - Crie um programa que leia e imprima 5 notas armazenadas em um vetor
+// int vetor [5];
+// for(int i = 0; i < 5; i++){
+//     cout << "Digite o valor para armazenar no vetor, posição " <<  i << "º : " << endl;
+//     cin >> vetor [i];
+// }
+// for(int i = 0; i < 5; i++){
+//     cout << " Valor armazenado na posição " << i << "º : " << vetor[i]<< endl;
+// }
 
 
 // matrizes - Implemente uma multiplicação de matrizes 2x2
 
-
+int matriz [2][2];
+for ( int i = 0; i < 2; i++ ){
+    for ( int j = 0; j < 2; j++ ){
+    cout << "Digite o valor para armazenar na posição [" << i << "][" << j << "]: ";
+    cin >> matriz[i][j];
+    }
+}
+//mostrar matriz
+cout << "Exibindo matriz digitada:\n";
+for( int i = 0; i < 2; i++ ){
+    for( int j = 0; j < 2; j++ ){
+    cout << matriz[i][j] << endl;
+    }
+}
 }
